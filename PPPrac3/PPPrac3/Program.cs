d8fea2f2733fbc4a032a88816b3f2b85c43729cd
streeexs <202380417@psu.palawan.edu.ph>
2024-09-21 13:04:58 +0800

    first commit

---
+++ PPPrac3/PPPrac3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PS_03_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckLeapYear(2025);
+        }
+
+        static void CheckLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} qualifies as a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} does not qualify as a leap year.");
+            }
+        }
+    }
+}
---
+++ Prac3/Prac3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Formats.Asn1;
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace Prac3
+{
+
+    public class HelloWorld
+    {
+
+        static double HeronsFormula(double e, double x, double r)
+        {
+            double s = (e + x + r) / 2;
+            double area = Math.Sqrt(s * (s - e) * (s - x) * (s - r));
+            return area;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter firt side of Triangle");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter second side of Triangle");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter third side of Triangle");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double result = HeronsFormula(e, x, r);
+
+            Console.WriteLine($"the area in triangle is {result}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+        
